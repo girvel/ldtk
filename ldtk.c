cf1e0e5d28bad444f21a4ba938df9f3a38846b5e
girvel <widauka@ya.ru>
2025-10-10 14:43:23 +0500

    Calculating world's width

--- ldtk.c
+++ ldtk.c
@@ -3,19 +3,51 @@
 #include <jansson.h>
 #include "nob.h"
 
+typedef struct {
+    int w, h;
+} Level;
+
+#define MAX2(A, B) ({ \
+    __typeof__ (A) _a = (A); \
+    __typeof__ (B) _b = (B); \
+    _a > _b ? _a : _b; \
+})
+
+#define MIN2(A, B) ({ \
+    __typeof__ (A) _a = (A); \
+    __typeof__ (B) _b = (B); \
+    _a < _b ? _a : _b; \
+}
+
 int main() {
     json_error_t error;
     json_t *root = json_load_file("../dot/levels/main/main.ldtk", 0, &error);
-
-    if (!root) {
-        fprintf(stderr, "error: on line %d: %s", error.line, error.text);
-        return 1;
-    }
-
     assert(json_is_object(root));
 
-    json_t *world_w_ptr = json_object_get(root, "worldGridWidth");
-    assert(json_is_integer(world_w_ptr));
+    json_t *levels = json_object_get(root, "levels");
+    assert(json_is_array(levels));
 
-    printf("worldGridWidth is %lld\n", json_integer_value(world_w_ptr));
+    Level fallen_level = {0};
+
+    size_t levels_n = json_array_size(levels);
+    for (size_t i = 0; i < levels_n; ++i) {
+        json_t *level = json_array_get(levels, i);
+        assert(json_is_object(level));
+
+        int offset_x = (int) ({
+            json_t *world_x_ptr = json_object_get(level, "worldX");
+            assert(json_is_integer(world_x_ptr));
+            json_integer_value(world_x_ptr);
+        }) / 16;
+
+        int w = (int) ({
+            json_t *world_x_ptr = json_object_get(level, "pxWid");
+            assert(json_is_integer(world_x_ptr));
+            json_integer_value(world_x_ptr);
+        }) / 16;
+
+        fallen_level.w = MAX2(fallen_level.w, offset_x + w);
+    }
+
+    printf("(%d, %d)\n", fallen_level.w, fallen_level.h);
 }
